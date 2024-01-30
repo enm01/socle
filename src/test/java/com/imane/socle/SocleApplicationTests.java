31075c8cec35a2d22610271e1d886d774651417e
imane <imaneziani2001@gmail.com>
2024-01-30 15:16:54 +0100

    code

---
+++ src/test/java/com/imane/socle/SocleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imane.socle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
